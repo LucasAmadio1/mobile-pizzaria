3891010a0e02c2630880b6bf2e1d961ac8a6df0f
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-03-27 22:50:56 -0300

    começo projeto

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import react from 'react'
+
+import { View, Text } from 'react-native'
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>
+        Tela de login
+      </Text>
+    </View>
+  )
+}
