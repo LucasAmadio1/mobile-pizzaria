b40e2d55f6f7f9dafaeefbc9a17ec806be36ee55
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-03-29 00:19:19 -0300

    controle de painas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import react from 'react'
+import { View, Text } from 'react-native'
+
+export default function Dashboard() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,105 @@
-import react from 'react'
+import react, { useState } from 'react'
 
-import { View, Text } from 'react-native'
+import { 
+  View, 
+  Text, 
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity
+} from 'react-native'
 
 export default function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleLogin() {
+    if (email === '' || password === '') {
+      return
+    }
+
+    alert('email:' + email)
+  }
+
   return (
-    <View>
-      <Text>
-        Tela de login
-      </Text>
+    <View style={styles.container}>
+      <Image 
+        style={styles.logo}
+        source={require('../../assests/logo.png')}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite seu email'
+          placeholderTextColor="#f0f0f0"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder='Digite sua senha'
+          placeholderTextColor="#f0f0f0"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>
+            Acessar
+          </Text>
+        </TouchableOpacity>
+
+      </View>
+
     </View>
   )
-}
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1d1d2e'
+  },
+
+  logo: {
+    marginBottom: 18,
+  },
+
+  inputContainer: {
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+
+  input: {
+    width: '95%',
+    height: 40,
+    backgroundColor: '#101026',
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: '#fff'
+  },
+
+  button: {
+    width: '95%',
+    height: 40,
+    backgroundColor: '#3fffa3',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#101026'
+  }
+})
